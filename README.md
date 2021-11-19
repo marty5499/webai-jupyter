d96467fdef76a894997db895dc148ddea2cd7f61
Marty <marty@kingkit101.com>
2021-11-19 23:41:27 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# webai-jupyter
+# Base Jupyter Notebook Stack
+
+[![docker pulls](https://img.shields.io/docker/pulls/jupyter/base-notebook.svg)](https://hub.docker.com/r/jupyter/base-notebook/)
+[![docker stars](https://img.shields.io/docker/stars/jupyter/base-notebook.svg)](https://hub.docker.com/r/jupyter/base-notebook/)
+[![image size](https://img.shields.io/docker/image-size/jupyter/base-notebook/latest)](https://hub.docker.com/r/jupyter/base-notebook/ "jupyter/base-notebook image size")
+
+GitHub Actions in the <https://github.com/jupyter/docker-stacks> project builds and pushes this image to Docker Hub.
+
+Please visit the project documentation site for help using and contributing to this image and others.
+
+- [Jupyter Docker Stacks on ReadTheDocs](https://jupyter-docker-stacks.readthedocs.io/en/latest/index.html)
+- [Selecting an Image :: Core Stacks :: jupyter/base-notebook](https://jupyter-docker-stacks.readthedocs.io/en/latest/using/selecting.html#jupyter-base-notebook)
+
+
+## 產生新的 image
+docker build -t jupyter/webai .
+
+## 執行 jupyter-webai
+docker run --name jupyter-webai -d -v /Users/marty/webai_board-builder/opt/jupyter-webai:/home/jovyan/— -p 8888:8888 jupyter/webai
+
+
+## enter container
+docker run --name jupyter-webai -e GRANT_SUDO=yes --user root -it -v /Users/marty/webai_board-builder/opt/jupyter-webai:/home/jovyan/— -p 8888:8888 jupyter/webai bash
---
+++ repl/main.js
@@ -0,0 +1,86 @@
+define([
+  'base/js/namespace',
+  'jquery',
+  'require',
+  'base/js/events',
+  'base/js/utils',
+  './repl'
+], function (Jupyter, $, requirejs, events, configmod, utils) {
+  "use strict";
+  var repl = new REPL();
+
+  repl.addListener(function(msg){
+    msg = msg.replace('\u0004\u0004>','');
+    console.log(msg);
+  })
+  console.log("repl>>>>:",repl);
+
+  var usbBtn,runBtn;
+
+  var load_extension = function () {    
+    var btns = Jupyter.toolbar.add_buttons_group([
+      Jupyter.keyboard_manager.actions.register({
+        'help': 'connect to Web:AI',
+        'icon': 'fa-usb',
+        'handler': async function () {
+          await repl.connectBoard();
+          usbBtn.style.backgroundColor='#aaffaa';
+        }
+      }, 'usb-connect', 'usbconnect'),
+      Jupyter.keyboard_manager.actions.register({
+        'help': 'enter REPL',
+        'icon': 'fa-cog',
+        'handler': async function () {
+            await repl.enterRAWREPL();
+            setTimeout(async function(){
+              await repl.sendCmd('from webai import *');
+              await repl.sendCmd('webai.init()');
+              await repl.sendCmd('webai.lcd.init()');
+              usbBtn.style.backgroundColor='#aaffaa';
+            },500);
+        }
+      }, 'usb-repl', 'usbrepl'),
+      Jupyter.keyboard_manager.actions.register({
+        'help': 'Run code',
+        'icon': 'fa-play',
+        'handler': async function () {
+          var nb = Jupyter.notebook;
+          var idx = nb.get_anchor_index();
+          var cell = nb.get_cell(idx);
+          var code = cell.get_text();
+          var output = '';
+          var startMsg = 'raw REPL; CTRL-B to exit\r\n>OK';
+          var startFlag = false;
+          repl.addListener(function(msg){
+            output += msg;
+            if(output.indexOf(startMsg)>=0){
+              output = output.replace(startMsg,'');
+              startFlag = true;
+            }
+            else if(startFlag){
+              output = output.replace('\u0004\u0004>','');
+              cell.output_area.clear_output();
+              cell.output_area.append_output({
+                  "output_type": "display_data",
+                  "metadata": {}, // included to avoid warning
+                  "data": {"text/plain": output}
+              });
+              //console.log(output);
+            }
+          })
+          await repl.sendCmd(code);
+        }
+      }, 'usb-run', 'usbrun'),
+    ]);
+
+    var usbBtn = btns.find('button')[0];
+    var runBtn = btns.find('button')[1];
+    usbBtn.style.backgroundColor='#ffaaaa';
+  };
+
+  var extension = {
+    load_jupyter_extension: load_extension,
+    load_ipython_extension: load_extension
+  };
+  return extension;
+});
